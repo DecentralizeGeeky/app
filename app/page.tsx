5411388831bdb933d1947c2091ab2db5d42e7d08
ADAMU MUHAMMAD MUHAMMAD <muhdadamuhd@gmail.com>
2024-10-04 21:27:32 +0100

    feat: frame-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { motion } from "framer-motion";
 import Hero from "./components/hero";
 import { Navbar } from "./components/Navbar";
 
@@ -5,8 +7,15 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Navbar />
-      <Hero />
+          <Navbar />
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="w-full"
+          >
+            <Hero />
+          </motion.div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
